daf1b7405d508f8592f35339ec38e8bbec838478
a-pompom <43694794+a-pompom@users.noreply.github.com>
2024-04-01 21:28:44 +0900

    Laravel学習-Routing Named Routeまで触った

---
+++ Backend/Laravel/serve.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker compose exec laravel php artisan serve --host 0.0.0.0
--- Backend/Laravel/src/routes/subRoutes/routing.php
+++ Backend/Laravel/src/routes/subRoutes/routing.php
@@ -13,5 +13,19 @@ Route::group([
     Route::get('/user/{id}', function (string $id) {
         return "Hello, {$id}";
     });
+
+    // Optional パラメータ
+    Route::get('/category/{name?}', function (string $name='all') {
+        return "Category is {$name}";
+    });
+
+    // Named Route
+    Route::get('/named-route/{id}', function (string $id) {
+        return "named route from {$id}";
+    })->name('named-route');
+
+    Route::get('/redirect-route', function () {
+        return redirect()->route('named-route', ['id' => 'redirectRoute']);
+    });
 });
 
